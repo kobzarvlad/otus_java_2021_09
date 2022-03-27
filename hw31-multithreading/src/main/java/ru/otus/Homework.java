573dcd589fde2c7b6f6ad487d79b4df6d42e5c18
kobzarvlad <kobzar_vladislav@mail.ru>
2022-03-27 15:32:21 +0300

    hw31-multithreading

--- hw31-multithreading/src/main/java/ru/otus/Homework.java
+++ hw31-multithreading/src/main/java/ru/otus/Homework.java
@@ -1,5 +1,39 @@
 package ru.otus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Homework {
 
+  private static final Logger log = LoggerFactory.getLogger(Homework.class);
+
+  private int prevThreadNumber = 1;
+
+  public static void main(String[] args) {
+    Homework homework = new Homework();
+    new Thread(() -> homework.count(0)).start();
+    new Thread(() -> homework.count(1)).start();
+  }
+
+  private void count(int threadNumber) {
+    for (int i = 1; i <= 10; i++) {
+      printValue(threadNumber, i);
+    }
+    for (int i = 9; i >= 1; i--) {
+      printValue(threadNumber, i);
+    }
+  }
+
+  private synchronized void printValue(int threadNumber, int value) {
+    while (prevThreadNumber == threadNumber) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+    log.info(String.valueOf(value));
+    prevThreadNumber = threadNumber;
+    notify();
+  }
 }
